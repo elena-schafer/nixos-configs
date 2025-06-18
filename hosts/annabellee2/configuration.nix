1386476314f23901a954789895364af92a223050
elena <elenaschafer12@gmail.com>
2025-06-18 16:59:02 +0000

    Cleaned up annabelle configuration.nix

--- hosts/annabellee2/configuration.nix
+++ hosts/annabellee2/configuration.nix
@@ -2,12 +2,13 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, lib, pkgs, home-manager, ... }:
+inputs@{ config, lib, pkgs, home-manager, ... }:
 
 {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+			../../modules/nixos
     ];
   
   networking.hostName = "annabellee2"; # Define your hostname.
