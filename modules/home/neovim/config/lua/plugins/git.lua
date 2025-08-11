e33d1aa92628ee59fd920586b6651dac6cebeee5
elena <elenaschafer12@gmail.com>
2025-08-10 18:26:53 -0600

    Got gitsigns working

--- modules/home/neovim/config/lua/plugins/git.lua
+++ modules/home/neovim/config/lua/plugins/git.lua
@@ -2,5 +2,9 @@ return {
 	{
 		"gitsigns.nvim",
 		lazy = false,
+		after = function()
+			-- docs say this isn't necessary, but it won't work without it for me
+			require("gitsigns").setup({})
+		end
 	},
 }
