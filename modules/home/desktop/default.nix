4a22634f2b3baba9921a54fa6eead6e356f38589
elena <elenaschafer12@gmail.com>
2025-08-16 17:55:26 -0600

    Added libreoffice

--- modules/home/desktop/default.nix
+++ modules/home/desktop/default.nix
@@ -1,36 +1,46 @@
-inputs@{ config, lib, pkgs, ... }:
+inputs@{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 let
-	cfg = config.desktop;
-in {
+  cfg = config.desktop;
+in
+{
   imports = [
     ./alacritty.nix
     ./firefox.nix
-		./hyprland
-		./streaming
+    ./hyprland
+    ./streaming
   ];
 
-	options.desktop = {
-		enable = lib.mkEnableOption "Enable Desktop Programs";
-	};
+  options.desktop = {
+    enable = lib.mkEnableOption "Enable Desktop Programs";
+  };
 
-	config = lib.mkIf cfg.enable {
-		services.easyeffects = {
-			enable = true;
-		};
-		home.packages = with pkgs; [
-			brave
+  config = lib.mkIf cfg.enable {
+    services.easyeffects = {
+      enable = true;
+    };
+    home.packages = with pkgs; [
+      brave
       (discord.override {
         withOpenASAR = true;
-				withVencord = true;
+        withVencord = true;
       })
-			krita
-			nerd-fonts.hack
-			pavucontrol
-			rofi-wayland
-			vlc
-			xfce.thunar
-			zoom-us
-		];
-	};
+      krita
+      nerd-fonts.hack
+      pavucontrol
+      rofi-wayland
+      vlc
+      xfce.thunar
+      zoom-us
+
+      libreoffice
+      hunspell # required for libreoffice spell check
+      hunspellDicts.en_US
+    ];
+  };
 }
