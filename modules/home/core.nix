12406f97f5ad307597428b54dd59277c712aeebb
elena <elenaschafer12@gmail.com>
2025-08-17 09:49:57 -0600

    Enable auto mounting removable devices

--- modules/home/core.nix
+++ modules/home/core.nix
@@ -2,9 +2,18 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, lib, pkgs, home-manager, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  home-manager,
+  ...
+}:
 
 {
-	imports = [ ./neovim ];
-}
+  imports = [ ./neovim ];
 
+  services.udiskie = {
+    enable = true;
+  };
+}
